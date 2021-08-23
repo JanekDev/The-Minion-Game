be1576a2883850985cd3192232cce2f1f48921cb
JanekDev <m.wilinski11@gmail.com>
2021-08-23 15:26:24 +0200

    fixed unit tests

--- src/test_game.py
+++ src/test_game.py
@@ -2,7 +2,7 @@ from game import minion_game
 
 #unit tests
 def test_game_diff():
-    assert minion_game("BANANA")=="Stuart 12"
-    assert minion_game("geek")=="Kevin 5"
+    assert minion_game("BANANA")=="Stuart is the winner with score 12."
+    assert minion_game("geek")=="Kevin is the winner with score 5."
 def test_game_draw():
     assert minion_game("BANAASA")=="Draw"
