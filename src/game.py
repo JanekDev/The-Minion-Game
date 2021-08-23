5e2aed08f3a6f3dfdbc2978b25e46df714e1057d
JanekDev <m.wilinski11@gmail.com>
2021-08-23 14:43:55 +0200

    main code commit - solved challenge

--- src/game.py
+++ src/game.py
@@ -1,5 +1,30 @@
-
+#Improved Hackerrank challenge solution by Michał Wiliński
+#Main loop definition
 def main():
-    pass
+    print(minion_game(input("Enter word:")))
+def minion_game(string):
+    #setting up variables
+    string=string.upper()
+    scoreboard={
+    "Kevin" : 0,
+    "Stuart" : 0
+    }
+    vowels="AEIOU"
+
+    #main loop for scoring
+    for i in range(len(string)):
+        points=len(string)-i
+        if string[i] in vowels:
+            scoreboard["Kevin"]+=points
+        else:
+            scoreboard["Stuart"]+=points
+    
+    #returning values
+    if len(scoreboard.values())==1:
+        return "Draw"
+    else:
+        winner=max(scoreboard,key=scoreboard.get)
+        score=scoreboard[winner]
+        return (winner+" "+str(score))
 if __name__=='__main__':
     main()
---
+++ src/test_game.py
@@ -0,0 +1,6 @@
+from game import minion_game
+
+#unit tests
+def test_game():
+    assert minion_game("BANANA")=="Stuart 12"
+    assert minion_game("geek")=="Kevin 5"
